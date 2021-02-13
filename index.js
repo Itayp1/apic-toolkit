63e7cd2a0ab04c4e5cbd96fe7b428f062fbe23b6
unknown <peretz.itay@gmail.com>
2021-02-13 23:37:37 +0200

    combine

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-// const { prompt } = require("enquirer");
-const fs = require("fs");
-const { Select } = require("enquirer");
+const index = require('./lib/index');
 
-const chooseOperation = new Select({
-  name: "operation",
-  message: "Choose a operation",
-  choices: ["yaml2scripts", "scripts2yaml"],
-});
-
-const chooseYamlFile = (files) =>
-  new Select({
-    name: "yamlFile",
-    message: "Choose a yamlFile",
-    choices: files,
-  });
-
-const start = async () => {
-  const response = await chooseOperation.run();
-  const files = fs.readdirSync("./").filter((name) => name.includes(".yaml"));
-  console.log(response);
-
-  const response2 = await chooseYamlFile(files).run();
-  console.log(response2);
-};
-
-start();
+module.exports = index;
